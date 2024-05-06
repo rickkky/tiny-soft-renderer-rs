59d38adc93626c509e72f6123bb820db176a048d
rickkky <rickkky@foxmail.com>
2024-05-06 09:16:19 +0800

    chore: pipeline temp

--- src/pipelilne.rs
+++ src/pipelilne.rs
@@ -1,2 +1,29 @@
 use crate::shader::ShaderProgramStruct;
 use interpolate::Interpolate;
+
+pub enum CullMode {
+    None,
+    Front,
+    Back,
+}
+
+pub enum DepthCompare {
+    Always,
+    Never,
+    Less,
+    LessEqual,
+    Greater,
+    GreaterEqual,
+    Equal,
+    NotEqual,
+}
+
+pub struct Pipeline<'a, V: Interpolate> {
+    pub program: &'a ShaderProgramStruct<'a, V>,
+
+    pub cull_mode: CullMode,
+
+    pub depth_write_enable: bool,
+
+    pub depth_compare: DepthCompare,
+}
